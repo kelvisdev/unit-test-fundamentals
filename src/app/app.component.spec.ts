88b5ac9dd4d248de66142b44594d9aff014db988
Kelvis Rodrigues Borges <kelvisbcc@gmail.com>
2022-05-22 20:10:00 -0300

    add test appComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'unit-tests-fundamentals'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('unit-tests-fundamentals');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('unit-tests-fundamentals app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'unit-tests-fundamentals app is running!'
+    );
+  });
+
+  it('Testando funcao de soma', () => {
+    const app = fixture.componentInstance;
+    expect(app.soma(2, 2)).toEqual(4);
+  });
+
+  it('Testando funcao de soma() dando valor errado', () => {
+    const app = fixture.componentInstance;
+    expect(app.soma(2, 2)).not.toEqual(2);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'unit-tests-fundamentals';
+
+  soma(valor1: number, valor2: number): number {
+    return valor1 + valor2;
+  }
 }
